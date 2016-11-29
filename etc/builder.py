70d2121fe0a46319f045cec648c9aa6c16998f3d
Giles Knap <giles.knap@diamond.ac.uk>
2016-11-29 09:04:54 +0000

    initial project - copy of gdaPlugins.xml from adUtil

--- etc/builder.py
+++ etc/builder.py
@@ -1,2 +1,10 @@
+from iocbuilder import Device, AutoSubstitution, SetSimulation, Xml
+from iocbuilder.arginfo import *
 
+from iocbuilder.modules.ADCore import ADCore, ADBaseTemplate, NDPluginBaseTemplate, includesTemplates, makeTemplateInstance
+from iocbuilder.modules.asyn import AsynIP, AsynPort
 
+class gdaPlugins(Xml):
+    """This plugin instantiates a standard set of plugins for use by GDA:"""
+    TemplateFile = 'gdaPlugins.xml'
+gdaPlugins.ArgInfo.descriptions["CAM"] = Ident("Areadetecor camera to connect to", ADCore)
